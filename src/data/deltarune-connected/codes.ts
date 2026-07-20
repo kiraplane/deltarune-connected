d97107274cf162bdb14e8b062670a53d10181110
贺（随云） <zhh0504@gmail.com>
2026-07-20 14:24:56 +0800

    chore: apply July 20 growth updates

--- src/data/deltarune-connected/codes.ts
+++ src/data/deltarune-connected/codes.ts
@@ -1,6 +1,6 @@
 import { officialGameFacts } from './sources';
 
-export const CODE_CHECKED_AT = '2026-07-19';
+export const CODE_CHECKED_AT = '2026-07-20';
 
 export const codeCheckSummary = {
   checkedAt: CODE_CHECKED_AT,
