bada87cca0d36b630f86aafd219e741683f97c2a
贺（随云） <zhh0504@gmail.com>
2026-08-13 21:17:18 +0800

    chore: refresh site growth data for 2026-08-13

--- src/data/deltarune-connected/codes.ts
+++ src/data/deltarune-connected/codes.ts
@@ -1,6 +1,6 @@
 import { officialGameFacts } from './sources';
 
-export const CODE_CHECKED_AT = '2026-08-11';
+export const CODE_CHECKED_AT = '2026-08-13';
 
 export const codeCheckSummary = {
   checkedAt: CODE_CHECKED_AT,
